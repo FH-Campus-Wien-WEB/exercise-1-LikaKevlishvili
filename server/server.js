60d3481cb3017858300b2a9851b048e426c080c2
Lika <lika.kevlishvili@hcw.ac.at>
2026-03-22 22:53:38 +0100

    1.Versuch

--- server/server.js
+++ server/server.js
@@ -2,16 +2,54 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-// Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
-// Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
-})
+    const movieData = [
+        {
+            "Title": "The Thing",
+            "Released": "1982-06-25",
+            "Runtime": 109,
+            "Genres": ["Horror", "Mystery", "Sci-Fi"],
+            "Directors": ["John Carpenter"],
+            "Writers": ["Bill Lancaster", "John W. Campbell Jr."],
+            "Actors": ["Kurt Russell", "Wilford Brimley", "Keith David"],
+            "Plot": "A research team in Antarctica is hunted by a shape-shifting alien that assumes the appearance of its victims.",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNGViZWZmM2EtNGYzZi00ZDAyLTk3ODMtNzIyZTBjN2Y1NmM1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+            "Metascore": 57,
+            "imdbRating": 8.2
+        },
+        {
+            "Title": "Inception",
+            "Released": "2010-07-16",
+            "Runtime": 148,
+            "Genres": ["Action", "Adventure", "Sci-Fi"],
+            "Directors": ["Christopher Nolan"],
+            "Writers": ["Christopher Nolan"],
+            "Actors": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+            "Plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+            "Metascore": 74,
+            "imdbRating": 8.8
+        },
+        {
+            "Title": "The Matrix",
+            "Released": "1999-03-31",
+            "Runtime": 136,
+            "Genres": ["Action", "Sci-Fi"],
+            "Directors": ["Lana Wachowski", "Lilly Wachowski"],
+            "Writers": ["Lana Wachowski", "Lilly Wachowski"],
+            "Actors": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+            "Plot": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+            "Metascore": 73,
+            "imdbRating": 8.7
+        }
+    ];
 
-app.listen(3000)
+    res.json(movieData);
+});
 
-console.log("Server now listening on http://localhost:3000/")
-
+app.listen(3000, function () {
+    console.log("Server now listening on http://localhost:3000/");
+});
